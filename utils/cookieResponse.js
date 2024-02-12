79d774203a13a8b7620b18f52a9b2096b4a6264c
root <root@yedu-dev.8.8.8.8>
2024-02-12 13:01:26 +0000

    some fixes

---
+++ utils/cookieResponse.js
@@ -0,0 +1,20 @@
+const STATIC = require("../static");
+
+const generateBearerCookie = (token, remember) => {
+  const duration = remember
+    ? STATIC.BEARER_REMEMBER_COOKIE_LIFETIME
+    : STATIC.BEARER_COOKIE_LIFETIME;
+
+  return {
+    name: "Bearer",
+    value: token,
+    options: {
+      maxAge: duration,
+      httpOnly: true
+    },
+  };
+};
+
+module.exports = {
+  generateBearerCookie,
+};
