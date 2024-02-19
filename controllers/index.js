1c6b271400a33a4f2f1ee6d697b3784e0e5a9bdc
Alex Verbovskiy <cofeek5@gmail.com>
2024-02-20 01:05:44 +0200

    some updates

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   userController: require("./userController"),
   logController: require("./logController"),
+  userEventLogController: require("./userEventLogController"),
   userVerifyRequestController: require("./userVerifyRequestController"),
+  systemController: require("./systemController"),
 };
