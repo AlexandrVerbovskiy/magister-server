fa24c6b9fc7bf20e1626f44d66ff41715ddd612c
root <root@yedu-dev.8.8.8.8>
2024-03-27 12:36:01 +0000

    some updates

--- utils/coordsByIp.js
+++ utils/coordsByIp.js
@@ -7,15 +7,22 @@ module.exports = async function coordsByIp(ip = null) {
       return STATIC.DEFAULT_LOCATION;
     }
 
+    ip = ip.split(", ")[0];
+
+    let data = null;
+
     const response = await fetch(
       `http://ip-api.com/json/${ip}?fields=${fields}`
     );
-    let data = await response.json();
+
+    const { lat, lon } = await response.json();
+    data = { lat, lng: lon };
 
     if (!data || Object.keys(data).length < 1) {
       const response2 = await fetch(
         `http://www.geoplugin.net/json.gp?ip=${ip}`
       );
+
       const { geoplugin_latitude = 0, geoplugin_longitude = 0 } =
         await response2.json();
 
@@ -36,7 +43,7 @@ module.exports = async function coordsByIp(ip = null) {
     if (!data || Object.keys(data).length < 1) {
       return STATIC.DEFAULT_LOCATION;
     }
-
+    
     return data;
   } catch (error) {
     console.error("Error getting IP data:", error);
