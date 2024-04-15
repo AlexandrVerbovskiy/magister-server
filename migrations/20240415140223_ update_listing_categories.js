24ac03096f9473fc5d7f11874b701bcc66890c94
Alex Verbovskiy <cofeek5@gmail.com>
2024-04-15 17:37:50 +0300

    some updates

---
+++ migrations/20240415140223_ update_listing_categories.js
@@ -0,0 +1,21 @@
+const STATIC = require("../static");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.LISTING_CATEGORIES, function (table) {
+    table.integer("order_index").nullable().defaultTo(null);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.LISTING_CATEGORIES, function (table) {
+    table.dropColumn("order_index");
+  });
+};
