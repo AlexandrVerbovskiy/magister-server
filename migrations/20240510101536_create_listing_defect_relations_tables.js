260f6f83b311f6be29d64663a911cdc83c5532b2
Alex Verbovskiy <cofeek5@gmail.com>
2024-05-10 15:00:00 +0300

    some updates

---
+++ migrations/20240510101536_create_listing_defect_relations_tables.js
@@ -0,0 +1,32 @@
+const STATIC = require("../static");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable(
+    STATIC.TABLES.LISTING_DEFECT_RELATIONS,
+    function (table) {
+      table.increments("id").primary();
+      table.timestamps(true, true);
+
+      table
+        .integer("listing_id")
+        .unsigned()
+        .references(STATIC.TABLES.LISTINGS + ".id");
+      table
+        .integer("listing_defect_id")
+        .unsigned()
+        .references(STATIC.TABLES.LISTING_DEFECTS + ".id");
+    }
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists(STATIC.TABLES.LISTING_DEFECT_RELATIONS);
+};
