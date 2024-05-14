ada8bb3d629a46f01e5b813d53802fc02d5e560f
Alex Verbovskiy <cofeek5@gmail.com>
2024-05-14 10:51:31 +0300

    some updates

--- controllers/senderPaymentController.js
+++ controllers/senderPaymentController.js
@@ -82,70 +82,74 @@ class SenderPaymentController extends Controller {
       return this.sendSuccessResponse(res, STATIC.SUCCESS.OK, null, result);
     });
 
-  generateInvoicePdf = async (req, res) => {
-    const { id } = req.params;
-    const userId = req.userData.userId;
-    const payment = await this.senderPaymentModel.getFullById(id);
+  generateInvoicePdf = (req, res) =>
+    this.baseWrapper(req, res, async () => {
+      const { id } = req.params;
+      const userId = req.userData.userId;
+      const payment = await this.senderPaymentModel.getFullById(id);
 
-    if (!payment || payment.payerId != userId) {
-      return res.send(404);
-    }
+      if (!payment || payment.payerId != userId) {
+        return res.send(404);
+      }
 
-    const offerStartDate = payment.orderOfferStartDate;
-    const offerEndDate = payment.orderOfferEndDate;
-    const offerPricePerDay = payment.orderOfferPricePerDay;
+      const offerStartDate = payment.orderOfferStartDate;
+      const offerEndDate = payment.orderOfferEndDate;
+      const offerPricePerDay = payment.orderOfferPricePerDay;
 
-    const offerTotalPrice = tenantPaymentCalculate(
-      offerStartDate,
-      offerEndDate,
-      payment.tenantFee,
-      offerPricePerDay
-    );
+      const offerTotalPrice = tenantPaymentCalculate(
+        offerStartDate,
+        offerEndDate,
+        payment.tenantFee,
+        offerPricePerDay
+      );
 
-    const offerSubTotalPrice =
-      getDaysDifference(offerStartDate, offerEndDate) * offerPricePerDay;
+      const offerSubTotalPrice =
+        getDaysDifference(offerStartDate, offerEndDate) * offerPricePerDay;
 
-    const factTotalFee = (offerSubTotalPrice * payment.tenantFee) / 100;
+      const factTotalFee = (offerSubTotalPrice * payment.tenantFee) / 100;
 
-    const durationString =
-      offerStartDate == offerEndDate
-        ? shortTimeConverter(offerStartDate)
-        : `${shortTimeConverter(offerStartDate)} - ${shortTimeConverter(
-            offerEndDate
-          )}`;
+      const durationString =
+        offerStartDate == offerEndDate
+          ? shortTimeConverter(offerStartDate)
+          : `${shortTimeConverter(offerStartDate)} - ${shortTimeConverter(
+              offerEndDate
+            )}`;
 
-    const params = {
-      billTo: payment.listingAddress ?? payment.listingCity,
-      shipTo: payment.listingAddress ?? payment.listingCity,
-      invoiceId: payment.id,
-      invoiceDate: shortTimeConverter(payment.createdAt),
-      purchaseOrder: payment.orderId,
-      dueDate: shortTimeConverter(payment.createdAt),
-      offer: {
-        factTotalPrice: offerTotalPrice.toFixed(2),
-        fee: payment.tenantFee,
-        listingName: payment.listingName,
-        pricePerDay: offerPricePerDay.toFixed(2),
-        subTotalPrice: offerSubTotalPrice.toFixed(2),
-        factTotalFee: factTotalFee.toFixed(2),
-        durationString,
-      },
-    };
+      const params = {
+        billTo: payment.listingAddress ?? payment.listingCity,
+        shipTo: payment.listingAddress ?? payment.listingCity,
+        invoiceId: payment.id,
+        invoiceDate: shortTimeConverter(payment.createdAt),
+        purchaseOrder: payment.orderId,
+        dueDate: shortTimeConverter(payment.createdAt),
+        offer: {
+          factTotalPrice: offerTotalPrice.toFixed(2),
+          fee: payment.tenantFee,
+          listingName: payment.listingName,
+          pricePerDay: offerPricePerDay.toFixed(2),
+          subTotalPrice: offerSubTotalPrice.toFixed(2),
+          factTotalFee: factTotalFee.toFixed(2),
+          durationString,
+        },
+      };
 
-    const destinationDir = path.join(STATIC.MAIN_DIRECTORY, "public/invoices");
-    const filename = destinationDir + `/inv-${payment.id}.pdf`;
+      const destinationDir = path.join(
+        STATIC.MAIN_DIRECTORY,
+        "public/invoices"
+      );
+      const filename = destinationDir + `/inv-${payment.id}.pdf`;
 
-    if (!fs.existsSync(destinationDir)) {
-      fs.mkdirSync(destinationDir, { recursive: true });
-    }
+      if (!fs.existsSync(destinationDir)) {
+        fs.mkdirSync(destinationDir, { recursive: true });
+      }
 
-    if (!fs.existsSync(filename)) {
-      const buffer = await this.generatePdf("/pdfs/invoice", params);
-      fs.writeFileSync(filename, buffer);
-    }
+      if (!fs.existsSync(filename)) {
+        const buffer = await this.generatePdf("/pdfs/invoice", params);
+        fs.writeFileSync(filename, buffer);
+      }
 
-    res.download(filename);
-  };
+      res.download(filename);
+    });
 }
 
 module.exports = new SenderPaymentController();
