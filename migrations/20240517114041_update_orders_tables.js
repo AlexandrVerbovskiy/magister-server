a397d49eeacecd73e5846b48e07637a1ea4f6930
Alex Verbovskiy <cofeek5@gmail.com>
2024-05-17 18:56:42 +0300

    some updates

---
+++ migrations/20240517114041_update_orders_tables.js
@@ -0,0 +1,23 @@
+const STATIC = require("../static");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.ORDERS, function (table) {
+    table.boolean("fee_active");
+    table.text("message");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.ORDERS, function (table) {
+    table.dropColumns("fee_active");
+    table.dropColumns("message");
+  });
+};
