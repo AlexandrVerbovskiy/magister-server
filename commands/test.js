859b2ceba671214026fd58d8ff0c28276a8aec42
root <root@yedu-dev.8.8.8.8>
2024-05-22 15:11:52 +0000

    some updates

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,8 @@
 const db = require("../database");
 
 const main = async () => {
-    const res = await db.raw(`SELECT created_at from listing_approval_requests`);
-    console.log(res)
+    const res = await db.raw(`select * from "listing_approval_requests" where listing_approval_requests.created_at >= '2024-05-10 00:00:00' and listing_approval_requests.created_at <= '2024-05-10 23:59:59' and "listing_approval_requests"."approved" is null`);
+    console.log(res.rows)
     process.exit();
 }
 
