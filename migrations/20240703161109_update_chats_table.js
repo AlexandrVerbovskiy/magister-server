a1cff70c74f18ae6bad6aea07575c5ce61e5528e
Alex Verbovskiy <cofeek5@gmail.com>
2024-07-03 19:12:21 +0300

    some updates

---
+++ migrations/20240703161109_update_chats_table.js
@@ -0,0 +1,21 @@
+const STATIC = require("../static");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.CHATS, function (table) {
+    table.text("name").alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.CHATS, function (table) {
+    table.string("name").alter();
+  });
+};
