ef804f5fde48da9087bfc95d36bbfd51cce9e923
verbovskiy.work@gmail.com <alex_verbovskiy@yedu-dev.8.8.8.8>
2024-07-31 08:30:28 +0000

    geocoding base

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,11 @@
-const db = require("../database");
+const fetch = require("node-fetch");
+const key = "AIzaSyB2tq-tFsqlE4rG7deKuxIh0QTTim0gFmk";
 
 const main = async () => {
-    const res = await db.raw(`SELECT created_at from listing_approval_requests`);
-    console.log(res)
-    process.exit();
-}
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=Lviv`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+};
 
 main();
