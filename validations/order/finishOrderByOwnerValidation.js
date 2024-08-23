4439feb470d1a433989f4896a95c8dc5a282c998
Alex Verbovskiy <cofeek5@gmail.com>
2024-08-23 15:48:35 +0300

    some updates

--- validations/order/finishOrderByOwnerValidation.js
+++ validations/order/finishOrderByOwnerValidation.js
@@ -2,4 +2,29 @@ const { validateSmallStringBody, validateBigStringBody } = require("../base");
 
 module.exports = [
   ...validateSmallStringBody({ field: "token", fieldName: "Token" }),
+  ...validateBigStringBody({
+    field: "itemMatchesDescription",
+    fieldName: "Item matches description",
+    required: false,
+  }),
+  ...validateBigStringBody({
+    field: "itemMatchesPhotos",
+    fieldName: "Item matches photos",
+    required: false,
+  }),
+  ...validateBigStringBody({
+    field: "itemFullyFunctional",
+    fieldName: "Item fully functional",
+    required: false,
+  }),
+  ...validateBigStringBody({
+    field: "partsGoodCondition",
+    fieldName: "Parts good condition",
+    required: false,
+  }),
+  ...validateBigStringBody({
+    field: "providedGuidelines",
+    fieldName: "Provided guidelines",
+    required: false,
+  }),
 ];
