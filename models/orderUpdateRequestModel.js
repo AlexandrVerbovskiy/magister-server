be5fc6762c74a7f7df83a065551eb9dacff9d50b
Alex Verbovskiy <cofeek5@gmail.com>
2024-10-27 18:56:42 +0200

    create orders

--- models/orderUpdateRequestModel.js
+++ models/orderUpdateRequestModel.js
@@ -8,29 +8,23 @@ const ORDER_UPDATE_REQUESTS_TABLE = STATIC.TABLES.ORDER_UPDATE_REQUESTS;
 class OrderUpdateRequestModel extends Model {
   visibleFields = [
     `${ORDER_UPDATE_REQUESTS_TABLE}.id`,
+    `${ORDER_UPDATE_REQUESTS_TABLE}.fee as newFee`,
     `${ORDER_UPDATE_REQUESTS_TABLE}.sender_id as senderId`,
     `${ORDER_UPDATE_REQUESTS_TABLE}.order_id as orderId`,
     `${ORDER_UPDATE_REQUESTS_TABLE}.active`,
     `${ORDER_UPDATE_REQUESTS_TABLE}.created_at as createdAt`,
     `${ORDER_UPDATE_REQUESTS_TABLE}.new_price as newPrice`,
-    `${ORDER_UPDATE_REQUESTS_TABLE}.new_start_time as newStartDate`,
-    `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as newFinishDate`,
+    `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as newFinishTime`,
   ];
 
-  create = async ({
-    orderId,
-    newStartDate,
-    newFinishDate,
-    newPrice,
-    senderId,
-  }) => {
+  create = async ({ orderId, newFinishTime, newPrice, senderId, fee }) => {
     const res = await db(ORDER_UPDATE_REQUESTS_TABLE)
       .insert({
         order_id: orderId,
         sender_id: senderId,
-        new_start_time: newStartDate,
-        new_finish_time: newFinishDate,
+        new_finish_time: newFinishTime,
         new_price: newPrice,
+        fee,
       })
       .returning("id");
 
@@ -73,8 +67,7 @@ class OrderUpdateRequestModel extends Model {
       .select([
         `${ORDER_UPDATE_REQUESTS_TABLE}.id as id`,
         `${ORDER_UPDATE_REQUESTS_TABLE}.new_price as price`,
-        `${ORDER_UPDATE_REQUESTS_TABLE}.new_start_time as startDate`,
-        `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as finishDate`,
+        `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as finishTime`,
         `${ORDER_UPDATE_REQUESTS_TABLE}.sender_id as senderId`,
       ])
       .where("order_id", orderId)
@@ -89,8 +82,7 @@ class OrderUpdateRequestModel extends Model {
       .select([
         `${ORDER_UPDATE_REQUESTS_TABLE}.id as id`,
         `${ORDER_UPDATE_REQUESTS_TABLE}.new_price as newPrice`,
-        `${ORDER_UPDATE_REQUESTS_TABLE}.new_start_time as newStartDate`,
-        `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as newFinishDate`,
+        `${ORDER_UPDATE_REQUESTS_TABLE}.new_finish_time as newFinishTime`,
         `${ORDER_UPDATE_REQUESTS_TABLE}.sender_id as senderId`,
       ])
       .where("order_id", orderId)
