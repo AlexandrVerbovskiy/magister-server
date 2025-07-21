b85eb266d930129adcd2b9c890d86ae6ebf35388
Alex Verbovskiy <cofeek5@gmail.com>
2025-07-21 21:55:21 +0300

    prediction started

--- routes/api/mainRoutes.js
+++ routes/api/mainRoutes.js
@@ -93,7 +93,7 @@ module.exports = (io) => {
     mainController.getAdminLogListPageOptions
   );
 
-    router.post(
+  router.post(
     "/admin-dispute-prediction-model-list-options",
     isAuth,
     isAdmin,
