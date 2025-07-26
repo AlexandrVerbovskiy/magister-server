9206547f51f795401b0f28e2ebc4076ad00d6429
Alex Verbovskiy <cofeek5@gmail.com>
2025-07-26 23:02:33 +0300

    dispute prediction started

---
+++ migrations/20250726125731_update_orders_tables.js
@@ -0,0 +1,21 @@
+const STATIC = require("../static");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable(STATIC.TABLES.ORDERS, function (table) {
+      table.integer("dispute_probability").nullable().defaultTo(null);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable(STATIC.TABLES.ORDERS, function (table) {
+      table.dropColumn("dispute_probability");
+  });
+};
